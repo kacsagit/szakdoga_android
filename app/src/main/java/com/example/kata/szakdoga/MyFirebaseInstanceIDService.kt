a4d1000e265a9b87439d8c99d6b5507b5bd1bfbc
kacsagit <katajona@gmail.com>
2017-10-18 11:59:15 +0200

    notif

---
+++ app/src/main/java/com/example/kata/szakdoga/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,28 @@
+package com.example.kata.szakdoga
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.messaging.FirebaseMessaging
+
+
+/**
+ * Created by Kata on 2017. 10. 17..
+ */
+class MyFirebaseInstanceIDService: FirebaseInstanceIdService() {
+
+    override fun onTokenRefresh() {
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: " + refreshedToken!!)
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+       // sendRegistrationToServer(refreshedToken)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kata/szakdoga/MyFirebaseMessagingService.kt
@@ -0,0 +1,75 @@
+package com.example.kata.szakdoga
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+/**
+ * Created by Kata on 2017. 10. 17..
+ */
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+    val NOTIFICATION_CHANNEL_ID = "4565"
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                //scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+                //handleNow()
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification.body!!)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.data["title"], remoteMessage.data["body"]);
+
+    }
+
+    private fun sendNotification(messageTitle: String?, messageBody: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val pattern = longArrayOf(500, 500, 500, 500, 500)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder = NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE, 1, 1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent) as NotificationCompat.Builder
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
